dfdb1e0b9a8c4ea97252b2f6552c14bc2410d342
snowdl <snowdl@naver.com>
2020-06-26 20:23:52 +0900

    update TodoInputForm  update TodoInputForm
        ### Type List -----------------------
        #   feat     | 기능 (새로운 기능)
        #   fix      | 버그 (버그 수정)
        #   refactor | 리팩토링
        #   style    | 스타일 (코드 형식, 세미콜론 추가: 비즈니스 로직에 변경 없음)
        #   docs     | 문서 (문서 추가, 수정, 삭제)
        #   test     | 테스트 (테스트 코드 추가, 수정, 삭제: 비즈니스 로직에 변경 없음)
        #   chore    | 기타 변경사항 (빌드 스크립트 수정 등)
    
        ### Scope List ----------------------
        #   atom     | atom 파일 변경
        #   molecule | molecule 파일 변경
        #   organism | organism 파일 변경
        #   template | template 파일 변경
        #   page     | page 파일 변경
        #   apollo   | apollo 관련 파일 변경
        #   story    | story 관련 수정
        #   etc      | etc...
        # ------------------------------------
    
        ### Etc  -----------------------------
        #  - 제목 첫 글자를 대문자로
        #  - 제목은 명령문으로
        #  - 제목 끝에 마침표(.) 금지
        #  - 제목과 본문을 한 줄 띄워 분리하기
        #  - 본문은 "어떻게" 보다 "무엇을", "왜"를 설명한다.
        #  - 본문에 여러줄의 메시지를 작성할 땐 "-"로 구분
        # ------------------------------------

--- src/components/TodoInput/TodoInputForm.jsx
+++ src/components/TodoInput/TodoInputForm.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
 const TodoInputForm = () => {
-  return <div>TodoInputForm</div>;
+  return (
+    <div class="ui menu">
+      <div class="item" bg-primary my-5>
+        <h3>todo list</h3>
+      </div>
+      <div className="card card-body my-2">
+        <form>
+          <div class="ui fluid icon input">
+            <input type="text" placeholder="To add..." />
+            <i class="circular plus icon"></i>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default TodoInputForm;
